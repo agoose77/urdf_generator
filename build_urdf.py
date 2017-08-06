5c33d524cb136b7a5acf79f85c4ba3c12a59940f
juliangaal <juliangaal@protonmail.com>
2017-08-07 01:22:07 +0200

    add ${...} evaluation for xacro property

--- build_urdf.py
+++ build_urdf.py
@@ -1,7 +1,7 @@
 import elements as em
 
 radius = em.Xacro_Property('radius', 0.56)
-diameter = em.Xacro_Property('diameter', radius.value*2)
+diameter = em.Xacro_Property('diameter', radius.name ,' * 2', True)
 box = em.Link('box', 'body', '0.5 0.7 0.1 1', '0.4 5 4', 'red')
 joint =	em.Joint('joint1', box.name, 'joint4', 'jointparent','0 0 0')#.say('hi')
 
@@ -10,6 +10,7 @@ xml = em.File(
 		radius, 
 		diameter,
 		em.Xacro_Include('/home/temp/test'),
+		em.Xacro_Include('/home2/temp2/test2'),
 		box,
 		joint
 	)
