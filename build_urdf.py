bcc2e557ce5dfd7e27f58180eafab4564d709284
juliangaal <juliangaal@protonmail.com>
2017-08-07 00:39:52 +0200

    make more flexible

--- build_urdf.py
+++ build_urdf.py
@@ -1,9 +1,17 @@
 import elements as em
 
-xml = em.File(
-		radius=em.Property('0.56'),
-		body=em.Link('box', '0.5 0.7 0.1 1', '0.4 5 4', 'red'),
-		joint1=em.Joint('box', 'joint4', 'jointparent','0 0 0')
+radius = em.Xacro_Property('radius', 0.56)
+diameter = em.Xacro_Property('diameter', radius.value*2)
+box = em.Link('box', 'body', '0.5 0.7 0.1 1', '0.4 5 4', 'red')
+joint =	em.Joint('joint1', box.name, 'joint4', 'jointparent','0 0 0')#.say('hi')
+
+
+xml = em.File( 
+		radius, 
+		diameter,
+		em.Xacro_Include('/home/temp/test'),
+		box,
+		joint
 	)
 
 xml.save()
