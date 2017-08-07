8d1156fd7feb6eb368d5ece40b899cef4b3440a1
Angus Hollands <goosey15@gmail.com>
2017-08-07 17:48:08 +0100

    Cleanup

--- build_urdf.py
+++ build_urdf.py
@@ -7,17 +7,16 @@ diameter = em.XacroProperty('diameter', radius * 2)
 box = em.Link('box', 'body', '0.5 0.7 0.1 1', '0.4 5 4', 'red')
 joint =	em.Joint('joint1', box, 'joint4', 'jointparent','0 0 0')#.say('hi')
 
-#
+
 xml = em.File(
 		em.XacroInclude('/home/temp/test'),
 		em.XacroInclude('/home2/temp2/test2'),
-
 		radius,
         diameter,
 		box,
 		joint,
 	)
-#
-# # ch.check(xml)
-#
+
+# ch.check(xml)
+
 xml.save()
